45eddcb78ccab6cc172f4120cd36f869c986ac37
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-10-11 22:13:22 +0530

    adding button to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -22,9 +22,9 @@ const Card = () => {
   }, []);
 
   return (
-    <div className=" bg-gradient-to-r from-pink-400 to-blue-700 grid lg:grid-cols-4 gap-8  grid-cols-3  p-8 my-10  md:grid-cols-3 sm:grid-cols-2 sm:gap-4 xs:grid-cols-1">
+    <div className=" bg-gradient-to-r from-pink-400 to-blue-700 grid lg:grid-cols-4 gap-8  grid-cols-1  p-8 my-10  md:grid-cols-3 sm:grid-cols-2 sm:gap-4">
       {pokemons.map((pokemon) => (
-        <div className="max-w-sm hover:scale-90   my-4  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm hover:scale-105   my-4  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <Link to={"/"}>
             <img
               className="rounded-t-lg w-64 h-56 object-cover text-center mx-auto"
@@ -38,7 +38,7 @@ const Card = () => {
                 {pokemon?.data?.name}
               </h5>
             </Link>
-            <div className="flex flex-col text-center my-5 lg:mx-32 md:mx-28 sm:mx-24 xs:mx-20  items-start   ">
+            <div className="flex flex-col text-center my-5 lg:mx-32 md:mx-28 mx-24 items-start   ">
               <h6 className="font-bold">
               
                 <span className=" text-blue-600"> Order:</span>
@@ -55,6 +55,12 @@ const Card = () => {
                 {pokemon?.data?.weight}
               </h6>
             </div>
+            <Link to={'/'} className="flex justify-center max-w-48  my-4 mx-auto items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </Link>
           </div>
         </div>
       ))}
